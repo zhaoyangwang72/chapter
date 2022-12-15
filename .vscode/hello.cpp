dd8fa975ba09bbfb21fe3e49e867976c86948b76
zhaoyangwang72 <jingdental@gmail.com>
2022-12-14 19:03:19 -0500

    adding ASCII comparison

---
+++ .vscode/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double num1 = 15, num2, sum;
+    num2 = 12;
+    sum = num1 + num2;
+    cout << " the sum is " << sum;
+    return 0;
+}
